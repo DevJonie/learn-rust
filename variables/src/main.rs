82e151061bd770b0f7226409fbc970d44ec5d2e4
john <john@test.com>
2024-02-09 11:28:20 +0000

    Init repo

---
+++ variables/src/main.rs
@@ -0,0 +1,11 @@
+
+const PI: f32 = 3.142;
+
+fn main (){
+    let mut x = 5;
+    println!("The value of x is : {x}");
+    x = 6;
+    println!("The value of x is : {x}");
+
+    println!("The value of PI is : {PI}")
+}
