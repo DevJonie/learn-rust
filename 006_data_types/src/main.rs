a843bbcb8fd8484dbc307e4d5951a2e035bbea03
devjonie <johnaddo.dev@gmail.com>
2024-02-12 14:39:10 +0000

    primitive data type

---
+++ 006_data_types/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let integer:u32 = 92_000; //DEcimal
+    let float:f32 = 3.142;
+    let hex = 0xff; // Hex
+    let oct = 0o77; // OCtal
+    let bin = 0b1111_0000; // binary
+    let byte = b'A'; // byte
+    let character:char = 'A'; //Char
+    let boolean:bool = false;
+}
