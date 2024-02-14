0dc66a8616c5d2854e35e856c05f3ba0a096b6be
devjonie <johnaddo.dev@gmail.com>
2024-02-14 08:36:20 +0000

    expression & statements & return values

--- 009_functions/src/main.rs
+++ 009_functions/src/main.rs
@@ -3,7 +3,13 @@ fn main(){
 
     another_function(10);
 
-    print_labeled_measurement(2, 'g')
+    print_labeled_measurement(2, 'g');
+
+    let five = five();
+    println!("Five = {five}");
+
+    let plus_one = plus_one(6);
+    println!("Plus one = {plus_one}");
 }
 //Function params
 fn another_function(x:i32){
@@ -12,4 +18,12 @@ fn another_function(x:i32){
 
 fn print_labeled_measurement(value: i32, unit_label: char){
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32{
+    5
+}
+
+fn plus_one(x:i32) -> i32{
+    return x + 1;
 }
